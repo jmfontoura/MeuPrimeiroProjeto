824adc6ab5cfd4daf845c6a5c1eacd0aa18b32f0
Jhonathan Fontoura <97266225+jmfontoura@users.noreply.github.com>
2026-05-21 20:15:26 -0300

    Add files via upload

---
+++ Projeto.cs
@@ -0,0 +1,16 @@
+﻿namespace HellowWorld;
+
+public class Programa
+{
+    static void Main()
+    {
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        var pessoa = new Humano(nome);
+        Console.WriteLine($"Seu nome é {pessoa.Nome}");
+
+
+
+    }
+}
